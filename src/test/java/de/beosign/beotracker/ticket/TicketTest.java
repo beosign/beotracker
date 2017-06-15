13a4b1c24e4db75df947427e68f2f334f81cb14c
Florian Dahlmanns <florian.dahlmanns@googlemail.com>
2017-06-15 18:44:35 +0200

    Test Omnifaces' o:validateBean

--- src/test/java/de/beosign/beotracker/ticket/TicketTest.java
+++ src/test/java/de/beosign/beotracker/ticket/TicketTest.java
@@ -1,15 +1,78 @@
 package de.beosign.beotracker.ticket;
 
-import org.junit.Test;
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TicketTest {
+import de.beosign.beotracker.ticket.Ticket.Priority;
+import de.beosign.beotracker.ticket.Ticket.Status;
+import de.beosign.beotracker.user.User;
+import de.beosign.beotracker.user.UserService;
+
+@Named
+@ViewScoped
+public class TicketTest implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private static final Logger log = LoggerFactory.getLogger(TicketTest.class);
 
-    @Test
-    public void test() {
-        log.info("Test");
+    @Inject
+    private TicketService ticketService;
+    @Inject
+    private UserService userService;
+
+    private List<User> users;
+    private Ticket ticket;
+
+    @PostConstruct
+    private void init() {
+        List<Ticket> tickets = ticketService.findAll();
+        if (tickets.size() == 0) {
+            ticket = new Ticket();
+        } else {
+            ticket = tickets.get(0);
+        }
+
+        users = userService.findAll();
+    }
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void onSave() {
+        log.info("Save called!");
+    }
+
+    public void setInvalid() {
+        ticket = new Ticket();
+        ticket.setStatus(Status.ASSIGNED);
+        ticket.setPriority(Priority.LOW);
+    }
+
+    public void setValid() {
+        ticket = new Ticket();
+        ticket.setStatus(Status.NEW);
+        ticket.setPriority(Priority.LOW);
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
 
 }
