d8abe7bedbc93cbb22f5751a4c78d4bc0958dc7e
Florian Dahlmanns <florian.dahlmanns@googlemail.com>
2017-10-21 18:05:15 +0200

    add ui:debug to verify components in tree

--- src/main/java/de/beosign/beotracker/foreach/ForEachBean.java
+++ src/main/java/de/beosign/beotracker/foreach/ForEachBean.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Named
 @ViewScoped
 public class ForEachBean implements Serializable {
     private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(ForEachBean.class);
     private List<Integer> items = new ArrayList<>();
 
     @PostConstruct
@@ -34,5 +39,10 @@ public class ForEachBean implements Serializable {
 
     public void setItems(List<Integer> items) {
         this.items = items;
+    }
+
+    public int getSum() {
+        log.debug("[" + FacesContext.getCurrentInstance().getCurrentPhaseId() + "] ForEachBean called");
+        return items.stream().reduce(0, (a, b) -> a + b);
     }
 }
